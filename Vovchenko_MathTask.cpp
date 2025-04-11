74826c3de57193a324b1803223eec362a02743b8
forele7 <mar_vov2008@mail.ru>
2025-04-11 09:06:13 +0300

    MathTask.h Added

---
+++ Vovchenko_MathTask.cpp
@@ -0,0 +1,14 @@
+#include "Vovchenko_MathTask.h"
+
+using namespace std;
+
+int main()
+{
+    int A = 0, B = 0;
+    Enter_Number(A, "Input height A: ");
+    Enter_Number(B, "Input width B: ");
+
+    int Rectangle = CalculateRectangle(A, B);
+
+    cout << "Area of Rectangle = " << Rectangle << endl;
+}
---
+++ Vovchenko_MathTask.h
@@ -0,0 +1,53 @@
+#ifndef _VOVCHENKO_MATHTASK_H_
+#define _VOVCHENKO_MATHTASK_H_
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <fstream>
+#include <numeric>
+#include <functional>
+#include <string>
+
+using namespace std;
+
+bool Input (string input)
+{
+    if (input.empty()) 
+    {
+        return false;
+    }
+    try
+    {
+        int number = stoi(input);
+        // if (number < 0)
+        // {
+        //     return false;
+        // }
+    }
+    catch (...)
+    {
+        return false;
+    }
+    return true;
+}
+
+void Enter_Number(int& varLink, string label)
+{
+        string get_input;
+        cout << label << "= ";
+        getline(cin, get_input);
+        while (!Input(get_input))
+        {
+            cout << label << "= ";
+            getline(cin, get_input);
+        }
+        varLink = stoi(get_input);
+};
+
+int CalculateRectangle(int A, int B)
+{
+    return A * B;
+}
+#endif
